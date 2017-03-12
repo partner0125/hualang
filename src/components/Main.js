7ac4de1dc55d681d56face5fa777fc36dc314fbe
Unknown <A1216485840@outlook.com>
2017-03-12 23:13:08 +0800

    图片组件大致雏形

--- src/components/Main.js
+++ src/components/Main.js
@@ -19,14 +19,36 @@ imageDatas = (function genImageURL(imageDatasArr) {
   return imageDatasArr;
 })(imageDatas);
 // imageDatas = genImageURL(imageDatas);
-
+let ImgFigure = React.createClass({
+  render: function () {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}
+          alt={this.props.data.title}
+        />
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+});
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [];
+    let imgFigures = [];
+
+    imageDatas.forEach(function (value) {
+      imgFigures.push(<ImgFigure data={value}/>);
+    });
+
     return (
       <section className="stage">
         <section className="img-sec">
+          {imgFigures}
         </section>
         <nav className="controller-nav">
+          {controllerUnits}
         </nav>
       </section>
     );
